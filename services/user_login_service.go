6fc7072cf6d2e2b7d1e7f223a78e88d1a84ac0f3
zl-php <zhoulei1@longtugame.com>
2022-04-24 17:51:40 +0800

    修改代码

--- services/user_login_service.go
+++ services/user_login_service.go
@@ -6,7 +6,7 @@ import (
 	"goapi/serializer"
 )
 
-// UserLoginService 管理用户登录的服务
+// 管理账号密码登录的服务
 type UserLoginService struct {
 	UserName string `form:"user_name" json:"user_name" binding:"required,min=5,max=30"`
 	Password string `form:"password" json:"password" binding:"required,min=8,max=40"`
